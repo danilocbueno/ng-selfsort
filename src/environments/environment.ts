5dd1d82296b9d83afa1a110e39c59aad6c61a51d
danilocbueno <danilocbueno@gmail.com>
2020-09-15 00:17:01 -0300

    firebase confs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB5aipDixsqH0mO0TjE5Tms3fdq71LNFMY",
+    authDomain: "self-sort.firebaseapp.com",
+    databaseURL: "https://self-sort.firebaseio.com",
+    projectId: "self-sort",
+    storageBucket: "self-sort.appspot.com",
+    messagingSenderId: "316097350679",
+    appId: "1:316097350679:web:31233af46b06a00209e052",
+    measurementId: "G-JEZ6JX37LY"
+  }
 };
 
 /*
