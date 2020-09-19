ff768c53f5b57a1d357037836ebcac82dbb7c913
danilocbueno <danilocbueno@gmail.com>
2020-09-19 10:44:30 -0300

    first deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+  { path: 'welcome', loadChildren: () => import('./welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: '',   redirectTo: '/welcome', pathMatch: 'full' },
+  //{ path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire'
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
-import { environment }  from '../environments/environment'
+import { environment }  from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {PublicModule} from "./public/public.module";
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { environment }  from '../environments/environment'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
+    PublicModule
   ],
   providers: [],
   bootstrap: [AppComponent]
